b04a70de3d5835cc38a3b0fa6aa457404e048949
unknown <Admin@MUTNS105.mut.ac.za>
2024-02-27 10:01:44 +0200

    Score

--- InterviewApplication/src/app/score-capture/score-capture.page.ts
+++ InterviewApplication/src/app/score-capture/score-capture.page.ts
@@ -4,9 +4,11 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { Router } from '@angular/router';
 import { AlertController, LoadingController, NavController, ToastController } from '@ionic/angular';
-import * as firebase from 'firebase/compat';
+//import * as firebase from 'firebase/compat';
 import 'firebase/auth';
 import { Observable, of } from 'rxjs';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 @Component({
   selector: 'app-score-capture',
@@ -14,7 +16,15 @@ import { Observable, of } from 'rxjs';
   styleUrls: ['./score-capture.page.scss'],
 })
 export class ScoreCapturePage implements OnInit {
+  groupedInterviewees: Map<string, any[]> = new Map();
 
+
+  int_id! :number;
+  name!:string;
+  surname!: string;
+  email!: string;
+  Status!: string;
+  Statuss: string = 'Interviewed';
   introduction: number = 0;
   teamwork: number = 0;
   overallImpression: number = 0;
@@ -27,9 +37,11 @@ export class ScoreCapturePage implements OnInit {
   label1= Text;
   label2=Text;
 
-  groupedInterviewees: Map<string, any[]> = new Map();
+
   todayDateString: string;
   selectedOption: any;// Variable to store the selected option
+  inProgressInterviewee: any = null; // Store the person with "In Progress" status
+
 
   data: any;
   tables$: any;
@@ -82,20 +94,26 @@ export class ScoreCapturePage implements OnInit {
   }
 
   ngOnInit() {
-    this.getAllDocuments2();
+    // this.getAllDocuments2();
+    this.fetchData();
   }
+
 
   
 
-  getAllDocuments2() {
-    this.firestore
-      .collection('Interviewees')
-      .valueChanges()
-      .subscribe((data) => {
-        this.tables$ = data;
-        console.log(this.tables$);
-      });
-  }
+  
+
+  
+
+  // getAllDocuments2() {
+  //   this.firestore
+  //     .collection('Interviewees')
+  //     .valueChanges()
+  //     .subscribe((data) => {
+  //       this.tables$ = data;
+  //       console.log(this.tables$);
+  //     });
+  // }
 
   calculateTotal() {
     this.total =
@@ -127,35 +145,84 @@ export class ScoreCapturePage implements OnInit {
 
   }
 
+  // submitForm() {
+  //    const selectedTable = this.tables$[this.selectedRowIndex];
+
+  //    if (!selectedTable) {
+  //     alert('Please select interview candidate.');
+  //   }else{
+
+  //     this.calculateTotal();
+
+  //   alert('Introduction:'+ this.introduction + '\n'
+  //   + 'Teamwork:'+ this.teamwork + '\n'
+  //    + 'Overall Impression:'+ this.overallImpression + '\n' 
+  //    + 'Leadership Skills:'+ this.leadershipSkills + '\n'
+  //    + 'Adaptability:'+ this.adaptability + '\n'
+  //    + 'Communication Skills:'+ this.communicationSkills + '\n'
+  //    + 'Job Specific Skills:'+ this.jobSpecificSkills + '\n'
+  //    + 'Problem-Solving:'+ this.problemSolving + '\n'
+  //    + 'Total:'+ this.total);
+
+  //    const {int_id, name} = selectedTable;
+
+  //    const label1Element = document.getElementById("label_1");
+  //    const label2Element = document.getElementById("label_2");
+
+  //     const label1 = label1Element ? label1Element.textContent : null;
+  //     const label2 = label2Element ? label2Element.textContent : null;
+
+  //   // Now you can proceed with adding the data to Firestore if needed
+  //   const formData = {
+  //     introduction: this.introduction,
+  //     teamwork: this.teamwork,
+  //     overallImpression: this.overallImpression,
+  //     leadershipSkills: this.leadershipSkills,
+  //     adaptability: this.adaptability,
+  //     communicationSkills: this.communicationSkills,
+  //     jobSpecificSkills: this.jobSpecificSkills,
+  //     problemSolving: this.problemSolving,
+  //     total: this.total,
+  //     ID: int_id,
+  //     name: name,
+  //     status: status,
+  //     panelMember: label1,
+  //     panelEmail: label2
+      
+  //   };
+
+  //   this.firestore
+  //     .collection('feedback')
+  //     .add(formData)
+  //     .then(() => {
+        
+  //       // Data added successfully
+  //       console.log('Form data added to Firestore!');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error adding form data to Firestore:', error);
+  //     });
+  //   }
+  // }
+
+
   submitForm() {
-     const selectedTable = this.tables$[this.selectedRowIndex];
-
-     if (!selectedTable) {
-      alert('Please select interview candidate.');
-    }else{
-
-      this.calculateTotal();
-
-    alert('Introduction:'+ this.introduction + '\n'
-    + 'Teamwork:'+ this.teamwork + '\n'
-     + 'Overall Impression:'+ this.overallImpression + '\n' 
-     + 'Leadership Skills:'+ this.leadershipSkills + '\n'
-     + 'Adaptability:'+ this.adaptability + '\n'
-     + 'Communication Skills:'+ this.communicationSkills + '\n'
-     + 'Job Specific Skills:'+ this.jobSpecificSkills + '\n'
-     + 'Problem-Solving:'+ this.problemSolving + '\n'
-     + 'Total:'+ this.total);
-
-     const {int_id, name} = selectedTable;
-
-     const label1Element = document.getElementById("label_1");
-     const label2Element = document.getElementById("label_2");
-
-      const label1 = label1Element ? label1Element.textContent : null;
-      const label2 = label2Element ? label2Element.textContent : null;
-
-    // Now you can proceed with adding the data to Firestore if needed
-    const formData = {
+    this.calculateTotal();
+    
+    
+    alert(`Introduction: ${this.introduction}\nTeamwork: ${this.teamwork}\n...\nName: ${this.name}`);
+    
+   
+    const stringData = {
+      name: this.name,
+      email: this.email,
+      Status: this.Statuss,
+      
+    };
+  
+    // Separate numeric values
+    const numericData = {
+      //int_id: this.int_id,
       introduction: this.introduction,
       teamwork: this.teamwork,
       overallImpression: this.overallImpression,
@@ -165,50 +232,97 @@ export class ScoreCapturePage implements OnInit {
       jobSpecificSkills: this.jobSpecificSkills,
       problemSolving: this.problemSolving,
       total: this.total,
-      ID: int_id,
-      name: name,
-      panelMember: label1,
-      panelEmail: label2
-      
+      // Include other numeric fields as needed
     };
-
+  
+    // Now, you can proceed with adding the data to Firestore
     this.firestore
       .collection('feedback')
-      .add(formData)
+      .add({
+        stringData,
+        numericData,
+      })
       .then(() => {
-        
         // Data added successfully
         console.log('Form data added to Firestore!');
       })
       .catch((error) => {
         console.error('Error adding form data to Firestore:', error);
       });
-    }
+      this.updateStatus();
   }
+ 
 
+  updateStatus() {
+    const intervieweeRef = this.firestore.collection('Interviewees', ref => ref.where('name', '==', this.name));
+    
+    intervieweeRef.get().subscribe((querySnapshot: firebase.firestore.QuerySnapshot<any>) => {
+      querySnapshot.forEach(doc => {
+        // Update the "Status" field
+        doc.ref.update({ Status: this.Statuss }) // Replace 'New Status' with the desired status
+          .then(() => {
+            alert('Status updated successfully');
+          })
+          .catch(error => {
+            console.error('Error updating status:', error);
+          });
+      });
+    });
+  }
+  
+  
   fetchData() {
-    this.firestore.collection('Interviewees').valueChanges().subscribe((data: any[]) => {
+    this.firestore.collection('Interviewees', ref => ref.where('Status', 'in', ['Waiting', 'In Progress'])).valueChanges().subscribe((data: any[]) => {
       this.groupedInterviewees = data.reduce((result, interviewee) => {
         const itemDate = new Date(interviewee.date);
         const dateKey = itemDate.toDateString();
-        
+  
         if (dateKey === this.todayDateString) {
-          //interviewee.date = 'Today';
-        } else {
           interviewee.date = dateKey;
+  
+          if (!result.has(dateKey)) {
+            result.set(dateKey, []);
+          }
+          result.get(dateKey).push(interviewee);
+  
+          // Check for "In Progress" status
+          if (interviewee.Status === 'Waiting') {
+            this.inProgressInterviewee = interviewee;
+          }
         }
   
-        if (!result.has(dateKey)) {
-          result.set(dateKey, []);
-        }
-        result.get(dateKey).push(interviewee);
-        
         return result;
       }, new Map<string, any[]>());
   
       // Sort the grouped interviewees by date
       this.sortIntervieweesByDate();
     });
+  }
+  
+
+  // Function to filter only today's data
+  getTodayData(): any[] {
+    return this.groupedInterviewees.get(this.todayDateString) || [];
+  }
+
+  start() {
+    if (this.inProgressInterviewee) {
+      const { name, surname, email, Status } = this.inProgressInterviewee;
+      this.name = name;
+      this.surname= surname;
+      this.email = email;
+      this.Status = Status;
+      // Display a message box with the details of the person with "In Progress" status
+      const confirmation = window.confirm(`Start interview with ${name} ${surname} (${email})?`);
+      if (confirmation) {
+        // Proceed with saving the data to the database
+        // this.submitForm();
+
+        console.log('The interview has started');
+      }
+    } else {
+      alert('No person with "In Progress" status found.');
+    }
   }
 
   Clear() {
