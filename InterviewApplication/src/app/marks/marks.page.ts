871109d9fed80750c948039078377f155b022603
SiputumaMthunzi <msiputuma@icloud.com>
2024-03-05 00:19:55 +0200

    grades

--- InterviewApplication/src/app/marks/marks.page.ts
+++ InterviewApplication/src/app/marks/marks.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 //import { Observable } from 'rxjs';
 
 @Component({
@@ -8,28 +9,29 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
   styleUrls: ['./marks.page.scss'],
 })
 export class MarksPage implements OnInit {
-  interviewData: any[] = [];
+  interviewData: any[] = []; // Ensure interviewData is initialized
 
-  constructor(//private menuCtrl: MenuController,
+  constructor(
     private firestore: AngularFirestore,
-     private db: AngularFirestore) 
-     {
-      this.getScoreData();
-      }
-  
-
-  ngOnInit() {
+    private db: AngularFirestore
+  ) {
+    this.getScoreData();
   }
-  
+
+  ngOnInit() {}
+
   getScoreData() {
     this.firestore.collection('feedback').valueChanges().subscribe((data: any[]) => {
       this.interviewData = data.map(item => ({
-        id: item.id,
-        intervieweeName: item.intervieweeName,
-        // intervieweeSurname: item.intervieweeSurname,
-        emailAddress: item.emailAddress,
-        score: item.score
+        name: item.stringData.name,
+        email: item.stringData.email,
+        total: item.numericData.total
       }));
     });
+  }
+}
+
+
   
-}}
+
+ 
