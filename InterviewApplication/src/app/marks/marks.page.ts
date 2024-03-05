55ae0a2adb63f34155d803ba3dda2cc0ef909f66
HlehleRh <hlehlerhamahle@gmail.com>
2024-03-05 14:46:11 +0200

    changes

--- InterviewApplication/src/app/marks/marks.page.ts
+++ InterviewApplication/src/app/marks/marks.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 //import { Observable } from 'rxjs';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-marks',
@@ -9,8 +10,9 @@ import { Observable } from 'rxjs';
   styleUrls: ['./marks.page.scss'],
 })
 export class MarksPage implements OnInit {
-  interviewData: any[] = []; // Ensure interviewData is initialized
-
+  interviewData: any[] = []; 
+  message: any = 'Congratulations on passing your interview';
+ 
   constructor(
     private firestore: AngularFirestore,
     private db: AngularFirestore
@@ -30,5 +32,29 @@ export class MarksPage implements OnInit {
             total: item.numericData.total
         }));
            });
+
+        
+           
 }
+async Send(email: string, name: string) {
+  const emailParams = {
+    email_to: email,
+    subject: 'Your Subject Here', 
+    message: 'Congratulations on passing your interview', 
+  };
+
+  try{
+    await emailjs.send('interviewEmailsAD','template_7x4kjte',
+    emailParams,'TrFF8ofl4gbJlOhzB'
+    );
+    console.log('email successfully sent');
+    alert('email successfully sent');
+ }
+catch(error){
+ console.error('error sending email', error);
+ alert('error sending email');
+}
+
+}
+
 }
