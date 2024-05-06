5a86020b258dfa3a200df1d625a6b9a6f959b345
MUTInnovationLab <mutinnovationlab@gmail.com>
2024-05-06 15:17:03 +0200

    error message

--- InterviewApplication/src/app/applicant-login/applicant-login.page.ts
+++ InterviewApplication/src/app/applicant-login/applicant-login.page.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import {  LoadingController,NavController, ToastController , AlertController} from '@ionic/angular';
+import { LoadingController, NavController, ToastController } from '@ionic/angular';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-applicant-login',
@@ -13,36 +9,35 @@ import { async } from '@angular/core/testing';
   styleUrls: ['./applicant-login.page.scss'],
 })
 export class ApplicantLoginPage implements OnInit {
-  email:any;
-  password:any;
+  email: any;
+  password: any;
   emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // regular expression for email validation
-  passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/; // regular expression for password validation
-  userData: any;
-  emailError: any;
-  passwordError: any;
+  passwordError: any; // Variable to hold the error message for the password
 
-  constructor( private db: AngularFirestore,private loadingController: LoadingController,
-     navCtrl: NavController,private auth: AngularFireAuth,private navController: NavController,
-     private toastController: ToastController) { }
+  constructor(
+    private db: AngularFirestore,
+    private loadingController: LoadingController,
+    private navCtrl: NavController,
+    private auth: AngularFireAuth,
+    private toastController: ToastController
+  ) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+
   goToHomePage(): void {
-    this.navController.navigateBack('/home');
+    this.navCtrl.navigateBack('/home');
   }
 
   reset() {
-    this.navController.navigateForward("/reset");
+    this.navCtrl.navigateForward("/reset");
   }
-  signUp(){
 
-    this.navController.navigateForward("/create");
-  
+  signUp() {
+    this.navCtrl.navigateForward("/create");
   }
-  goToSignUp(){
 
-    this.navController.navigateForward("/applicant-resgister");
-  
+  goToSignUp() {
+    this.navCtrl.navigateForward("/applicant-resgister");
   }
 
   async validate() {
@@ -56,7 +51,7 @@ export class ApplicantLoginPage implements OnInit {
       toast.present();
       return;
     }
-  
+
     // Validate email format
     if (!this.emailRegex.test(this.email)) {
       const toast = await this.toastController.create({
@@ -67,8 +62,9 @@ export class ApplicantLoginPage implements OnInit {
       toast.present();
       return;
     }
-  
-    // Validate password format
+
+    // Validate password
+
     if (!this.password) {
       const toast = await this.toastController.create({
         message: 'Please enter your password.',
@@ -78,111 +74,83 @@ export class ApplicantLoginPage implements OnInit {
       toast.present();
       return;
     }
-  
-    if (!this.passwordRegex.test(this.password)) {
-      const toast = await this.toastController.create({
-        message: 'Password must contain at least 8 characters including uppercase, lowercase, and numbers.',
-        duration: 2000,
-        color: 'danger'
-      });
-      toast.present();
-      return;
-    }
-  
+
     // If all validations pass, continue with sign-in logic
     this.log();
   }
-  
-  
-  
-  
-
 
   async log() {
-
-    this.emailError = null;
     this.passwordError = null;
-   
+
     if (!this.email) {
-      this.emailError = 'Please enter your email.';
       alert("Please enter your email");
       return;
     }
 
     if (!this.password) {
-      this.passwordError = 'Please enter a password.';
       alert("Please enter a password");
       return;
     }
 
-      const loader = await this.loadingController.create({
-        message: 'Signing in',
-        cssClass: 'custom-loader-class'
-      });
-      await loader.present();
+    const loader = await this.loadingController.create({
+      message: 'Signing in',
+      cssClass: 'custom-loader-class'
+    });
+    await loader.present();
 
-      this.auth.signInWithEmailAndPassword(this.email, this.password)
-      .then(( userCred ) => {
+    this.auth.signInWithEmailAndPassword(this.email, this.password)
+      .then((userCred) => {
+        if (userCred) {
+          this.db.collection('registeredStudents', ref => ref.where('email', '==', this.email))
+            .get()
+            .toPromise()
+            .then((querySnapshot: any) => {
+              querySnapshot.forEach((doc: { id: any; data: () => any; }) => {
+                const id = doc.id;
+                const userData = doc.data();
+                const loginCount = userData.loginCount || 0;
+                const newLoginCount = loginCount + 1;
 
-        if (userCred){
+                this.db.collection("registeredStudents").doc(id).update({ loginCount: newLoginCount });
+              });
+            });
 
-        this.db.collection('registeredStudents', ref => ref.where('email', '==', this.email))
-        .get()
-        .toPromise()
-        .then((querySnapshot:any) => {
-          querySnapshot.forEach((doc: { id: any; data: () => any; }) => {
-            const id = doc.id;
-            console.log(id);
-            const userData = doc.data();
-            const loginCount = userData.loginCount || 0;
-            const newLoginCount = loginCount + 1;
-
-            this.db.collection("registeredStudents").doc(id).update({ loginCount: newLoginCount });
-          });
-        });
-
-
-
-
-
-
-     this.db.collection("registeredStudents")
-          .ref.where("email", "==", this.email.trim())
-          .get()
-          .then((querySnapshot) => {
-            loader.dismiss();
-            if (!querySnapshot.empty) {
-              this.navController.navigateForward("/posts");
-           
-            } else {
-              this.navController.navigateForward("/dashboard");
-             
-            }
-          })
-          .catch((error) => {
-            loader.dismiss();
-            const  errorMessage= error.message;
-            
-          
-          });}
+          this.db.collection("registeredStudents")
+            .ref.where("email", "==", this.email.trim())
+            .get()
+            .then((querySnapshot) => {
+              loader.dismiss();
+              if (!querySnapshot.empty) {
+                this.navCtrl.navigateForward("/posts");
+              } else {
+                this.navCtrl.navigateForward("/dashboard");
+              }
+            })
+            .catch((error) => {
+              loader.dismiss();
+              const errorMessage = error.message;
+            });
+        }
       })
-    
-      .catch((error) => {
+      .catch(async (error) => {
         loader.dismiss();
         const errorMessage = error.message;
         if (errorMessage === "Firebase: The password is invalid or the user does not have a password. (auth/wrong-password)." 
         || errorMessage === "Firebase: There is no user record corresponding to this identifier. The user may have been deleted. (auth/user-not-found).") {
-          alert("Invalid email or password");
+    
+
+          const toast =  this.toastController.create({
+            message: 'Invalid email or password',
+            duration: 2000,
+            color: 'danger'
+          });
+          (await toast).present();
+          return;
         } else if (errorMessage === "Firebase: The email address is badly formatted. (auth/invalid-email).") {
-          const errorMessage = error.message;
-          alert("incorrectlly formated email");
+          alert("incorrectly formatted email");
         }
       });
-    
   }
-
-
-
   
   async getUserData(email: string) {
     try {
